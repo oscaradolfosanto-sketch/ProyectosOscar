aa99e66fae0c8c5092ac15826d2a822b46fc6eb5
oscaradolfosanto-sketch <oscaradolfosanto@gmail.com>
2026-06-01 21:04:49 +0200

    Entrega
    subir archivos que no se subieron

---
+++ ejercicio/ejercicio/Program.cs
@@ -0,0 +1,15 @@
+﻿1using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
